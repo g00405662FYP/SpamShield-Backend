8a098727d885d36fcc91263fab48fbea4e7189e3
CormacM22 <g00405662@atu.ie>
2024-11-18 10:52:10 +0000

    Succesfully Connected Backend And Frontend

--- myproject/app.py
+++ myproject/app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Allow CORS for frontend-backend communication
 
-@app.route('/')
-def home():
-    return 'Hello from Flask'
+@app.route('/hello', methods=['GET'])
+def hello():
+    return jsonify(message="Hello from Flask backend!")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
